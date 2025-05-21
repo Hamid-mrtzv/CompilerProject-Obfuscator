54267b0d0ba2191ea0889e3b7f8d0437c3a6cf25
Hamidreza <hamid.mrtzv.official@gmail.com>
2025-05-21 17:26:05 +0330

    feat : implemented Variable Renamer

---
+++ src/main/java/obfuscator/VariableRenamer.java
@@ -0,0 +1,27 @@
+package obfuscator;
+
+import java.util.regex.*;
+import java.util.*;
+
+public class VariableRenamer {
+    public static String rename(String code) {
+        Map<String, String> map = new HashMap<>();
+        Set<String> reserved = Set.of("main", "printf", "scanf", "return", "if", "else", "while", "for", "true", "false");
+
+        Matcher m = Pattern.compile("\\b(int|char|bool)\\s+(\\w+)").matcher(code);
+        int counter = 0;
+
+        while (m.find()) {
+            String var = m.group(2);
+            if (!reserved.contains(var)) {
+                map.putIfAbsent(var, "v" + counter++);
+            }
+        }
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            code = code.replaceAll("\\b" + Pattern.quote(entry.getKey()) + "\\b", entry.getValue());
+        }
+
+        return code;
+    }
+}
